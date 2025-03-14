498f80be89033ec97d66da878771a76bf357545c
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2025-03-14 01:58:26 -0300

    refactor: substitui lógica de logout para uso do contexto de autenticação, simplificando a implementação do botão de logout

--- src/components/LogoutButton/index.tsx
+++ src/components/LogoutButton/index.tsx
@@ -1,24 +1,11 @@
-import { auth } from '@/config/firebase/firebase'; // Certifique-se de que o caminho está correto
-import { signOut } from 'firebase/auth';
-import { useRouter } from 'next/router';
+import { useAuth } from '@/contexts/AuthContext';
 
 export const LogoutButton = () => {
-  const router = useRouter();
-
-  const handleLogout = async () => {
-    try {
-      await signOut(auth);
-      console.log('Usuário deslogado com sucesso!');
-      router.replace('/customer/login');
-      // Redirecionar para a tela de login, se necessário
-    } catch (error) {
-      console.error('Erro ao deslogar:', error);
-    }
-  };
+  const { logout } = useAuth();
 
   return (
     <button
-      onClick={handleLogout}
+      onClick={logout}
       className='bg-red-500 text-white px-4 py-2 rounded'
     >
       Sair
