e2cf94cdf7e469ae956c3ee1a74abd2553123b6f
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2025-04-10 23:25:31 -0300

    feat: ajusta o layout do componente ComandaButtons, adicionando altura fixa e margens, para melhorar a apresentação e a usabilidade dos botões na interface.

--- src/components/ComandaButtons/index.tsx
+++ src/components/ComandaButtons/index.tsx
@@ -8,11 +8,11 @@ const ComandaButtons: React.FC = () => {
   const right_button_class = 'rounded-r-md rounded-l-none';
   const middle_button_class = 'rounded-none';
   const shared_button_class =
-    'place-items-center flex flex-row items-center gap-[10px] justify-center';
+    'place-items-center flex flex-row items-center gap-[10px] justify-center h-[48px]';
 
   return (
     <MaxWidthLayout>
-      <div className='grid grid-cols-3 gap-[2px] w-full max-w-[500px] py-[20px]'>
+      <div className='grid grid-cols-3 gap-[2px] w-full max-w-[500px] my-[20px] h-[48px]'>
         {conta_buttons_navigation.map((button, index) => (
           <Button
             key={button.href}
