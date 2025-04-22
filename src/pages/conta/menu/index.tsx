48545eb393d365db620c3eda39f9c3fafa3225f9
Joao Vitral <joaoviniciusvitral@hotmail.com>
2025-04-21 22:59:28 -0300

    refatora a exibição dos itens do menu na página de conta, melhorando a formatação do código e a legibilidade, além de garantir a consistência na apresentação dos elementos.

--- src/pages/conta/menu/index.tsx
+++ src/pages/conta/menu/index.tsx
@@ -40,19 +40,30 @@ const MenuDaConta: React.FC = () => {
       <MaxWidthWrapper>
         <div className='grid grid-cols-3 md:grid-cols-4 gap-x-[16px] gap-y-[12px]'>
           {menu.map((item) => (
-            <div key={item.id} className='flex flex-col rounded-[8px] bg-[#F5F5F5] shadow-[0px_4px_4px_0px_#00000040] w-full' >
-              <div className='relative w-full' style={{
-                aspectRatio: 1.09
-              }}>
-                <Image src={item.imagem || '/products/beer.webp'} alt={item.nome} layout='fill' objectFit='cover' />
+            <div
+              key={item.id}
+              className='flex flex-col rounded-[8px] bg-[#F5F5F5] shadow-[0px_4px_4px_0px_#00000040] w-full'
+            >
+              <div
+                className='relative w-full'
+                style={{
+                  aspectRatio: 1.09,
+                }}
+              >
+                <Image
+                  src={item.imagem || '/products/beer.webp'}
+                  alt={item.nome}
+                  layout='fill'
+                  objectFit='cover'
+                />
               </div>
               <div className='flex flex-col gap-y-[4px] p-[4px]'>
-                <span className='text-[12px] font-[600] leading-[16px] tracking-[0.5px] text-[#000000]'>{capitalize(item.nome)}</span>
-                <span className='text-[11px] font-[500] leading-[14px] tracking-[0.5px] text-[#000000]'>{
-                  currencyFormatter(
-                    item.preco
-                  )
-                }</span>
+                <span className='text-[12px] font-[600] leading-[16px] tracking-[0.5px] text-[#000000]'>
+                  {capitalize(item.nome)}
+                </span>
+                <span className='text-[11px] font-[500] leading-[14px] tracking-[0.5px] text-[#000000]'>
+                  {currencyFormatter(item.preco)}
+                </span>
               </div>
             </div>
           ))}
