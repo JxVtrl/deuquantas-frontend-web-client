e564c11cbf8946eaa8582b6b22b89723590e6e1a
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2025-04-28 11:42:12 -0300

    implementa a exibição de itens na lista de comandas, utilizando o contexto ComandaContext e um wrapper de largura máxima para melhor apresentação dos dados.

--- src/components/Comanda/List/index.tsx
+++ src/components/Comanda/List/index.tsx
@@ -1,5 +1,21 @@
+import { useComanda } from '@/contexts/ComandaContext';
+import { MaxWidthWrapper } from '@deuquantas/components';
 import React from 'react';
 
 export const ComandaList: React.FC = () => {
-  return <div>ComandaList</div>;
+  const { comanda } = useComanda();
+
+  const itens = comanda?.itens;
+
+  return (
+    <MaxWidthWrapper>
+      <div>
+        {itens?.map((item) => (
+          <div key={item.id}>
+            <div>{item.nome}</div>
+          </div>
+        ))}
+      </div>
+    </MaxWidthWrapper>
+  );
 };
