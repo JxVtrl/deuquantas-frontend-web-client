21f4f98e0db0d14d6dd4454e937dc38ab422324a
João Vinicius Vitral <65374802+JxVtrl@users.noreply.github.com>
2025-04-28 11:38:38 -0300

    adiciona botão de opções de pagamento ao componente ComandaPayOptions, melhorando a interface com um wrapper de largura máxima e estilização adequada.

--- src/components/Comanda/PayOptions/index.tsx
+++ src/components/Comanda/PayOptions/index.tsx
@@ -1,5 +1,22 @@
+import { Button } from '@deuquantas/components';
+import { MaxWidthWrapper } from '@deuquantas/components';
 import React from 'react';
 
 export const ComandaPayOptions: React.FC = () => {
-  return <div>ComandaPayOptions</div>;
+  return (
+    <MaxWidthWrapper
+      style={{
+        paddingBottom: '90px',
+      }}
+    >
+      <Button
+        variant='primary'
+        text='OPÇÕES DE PAGAMENTO'
+        style={{
+          width: '100%',
+          marginTop: '32px',
+        }}
+      />
+    </MaxWidthWrapper>
+  );
 };
